e14dcbfe0b463239d3993e29420d811be6c6ff7d
Ollie Bannister <olwiba@gmail.com>
2016-06-20 23:44:26 +1200

    Created a landing page and began to scaffold out views

--- index.js
+++ index.js
@@ -12,7 +12,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', function (req, res) {
+  res.render('landing-page');
+});
+
+app.get('/welcome', function (req, res) {
+	console.log('this is the request', req.query.visitor)
+	res.render('index');
+});
+
+app.get('/tour', function (req, res) {
+	console.log('this is the request', req.query)
+	res.render('index');
+});
+
+app.get('/home', function (req, res) {
   res.render('index');
+});
+
+app.get('/test', function (req, res) {
+  res.render('test');
 });
 
 app.listen(1337, function () {
---
+++ public/js/welcome.js
@@ -0,0 +1,31 @@
+$('#askName').hide();
+
+var progress = setInterval(function () {
+    var $bar = $("#bar");
+
+    if ($bar.width() >= 600) {
+        clearInterval(progress);
+    } else {
+        $bar.width($bar.width() + 60);
+    }
+    $bar.text($bar.width() / 6 + "%");
+    if ($bar.width() / 6 == 100){
+      $bar.text("Still working ... " + $bar.width() / 6 + "%");
+    }
+}, 800);
+
+$(window).load(function() {
+  $("#bar").width(600);
+  $(".loader").fadeOut(2000);
+
+      $('#takeTour').click(function () {
+        $('#main').hide();
+        $('#askName').fadeIn('slow');     
+    });
+});
+
+// $(document).ready(function () {
+
+
+
+// });
